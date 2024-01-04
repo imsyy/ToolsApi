081a3a71e1e5ac6714cc422c3e49eaaaeef31cf5
imsyy <one@imsyy.top>
2024-01-04 10:48:52 +0800

    🐞 fix: 修复图片缓存路径

--- routes/bing.js
+++ routes/bing.js
@@ -137,7 +137,7 @@ bingRouter.get("/bing/image", async (ctx) => {
 });
 
 // 本地图片缓存目录
-const cacheDir = path.join(__dirname, "images");
+const cacheDir = path.join(process.cwd(), "images");
 if (!fs.existsSync(cacheDir)) {
   fs.mkdirSync(cacheDir);
 }
